68d4c3f900e055fe43e45d6f56764ca6b8129ce4
Gavin Lazar Suntop <gavin@gsuntop.com>
2015-06-29 10:52:03 -0700

    removing config.js

--- config.js
+++
@@ -1,2 +0,0 @@
-// THIS IS A GENERATED FILE. EDIT npm_tasks/build-config.js INSTEAD
-module.exports = {"CLIENT_ID":"wm_id_zIPGbkEDB5Cv9dCzo7nS","API_URI":"https://webmaker-api.herokuapp.com","LOGIN_URI":"https://id.mofostaging.net"};
