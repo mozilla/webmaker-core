fb6582b59d1301008b0539a5d0e70b50967e46cf
Gavin Lazar Suntop <gavin@gsuntop.com>
2015-07-17 13:26:28 -0700

    two styles of dpad buttons w. hard coded variants

--- src/components/d-pad/d-pad.jsx
+++ src/components/d-pad/d-pad.jsx
@@ -19,13 +19,14 @@ module.exports = React.createClass({
   bulkSetVisibility: function (newState) {
     this.setState(newState);
   },
+  // TODO: Add "moon logic". Switch between moon and nub style buttons depending on available space.
   render: function () {
     return (
-      <div className={'dPad moons' + (this.props.isVisible ? '' : ' hidden')}>
+      <div className={'dPad' + (this.props.isVisible ? '' : ' hidden')}>
         <button className={'up' + (this.state.showUp ? '' : ' hidden')} onClick={this.onButtonClick.bind(this, 'up')}></button>
         <button className={'down' + (this.state.showDown ? '' : ' hidden')} onClick={this.onButtonClick.bind(this, 'down')}></button>
-        <button className={'left' + (this.state.showLeft ? '' : ' hidden')} onClick={this.onButtonClick.bind(this, 'left')}></button>
-        <button className={'right' + (this.state.showRight ? '' : ' hidden')} onClick={this.onButtonClick.bind(this, 'right')}></button>
+        <button className={'left moon' + (this.state.showLeft ? '' : ' hidden')} onClick={this.onButtonClick.bind(this, 'left')}></button>
+        <button className={'right moon' + (this.state.showRight ? '' : ' hidden')} onClick={this.onButtonClick.bind(this, 'right')}></button>
       </div>
     );
   }
