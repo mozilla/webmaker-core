2a9ca87cae879a35ae1aeeab99b8bd3b534e9ec1
Ricardo Vazquez <ricardo@mozillafoundation.org>
2015-08-27 14:12:21 -0400

    Changing username or email label
    Fixes #607

--- src/pages/login/sign-in.jsx
+++ src/pages/login/sign-in.jsx
@@ -49,7 +49,7 @@ var SignIn = React.createClass({
   fields: [
     {
       name: 'username',
-      label: 'username',
+      label: 'username_email',
       type: 'email',
       tabIndex: 1,
       required: true
