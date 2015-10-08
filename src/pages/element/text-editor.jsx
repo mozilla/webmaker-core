a2bb39d914f0faa960ff3c51d4fd7603c0ecc637
Alan Mooiman <amooiman@gmail.com>
2015-10-08 12:03:51 -0400

    Align link/text colors/labels
    Issues #55, #611

--- src/pages/element/text-editor.jsx
+++ src/pages/element/text-editor.jsx
@@ -80,10 +80,10 @@ var TextEditor = React.createClass({
             </div>
             <div className="form-group">
 	      <label>{this.getIntlMessage('color')}</label>
-              <ColorGroup id="color" linkState={this.linkState} colors={colorChoices} params={this.props.params} onLaunchTinker={this.props.save} />
-            </div>
-            <div className="form-group">
-	      <label>{this.getIntlMessage('background_color')}</label>
+	      <ColorGroup id="color" linkState={this.linkState} colors={colorChoices} params={this.props.params} onLaunchTinker={this.props.save} />
+	    </div>
+	    <div className="form-group">
+	      <label>{this.getIntlMessage('background')}</label>
 	      <ColorGroup id="backgroundColor" linkState={this.linkState} params={this.props.params} onLaunchTinker={this.props.save} />
 	    </div>
 	    <div className="form-group">
